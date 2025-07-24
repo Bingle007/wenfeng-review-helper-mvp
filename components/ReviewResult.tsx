a8beb06e2667fd5d9edeeae3007f8c2417d0cbe9
Bingle007 <133489117+Bingle007@users.noreply.github.com>
2025-07-24 17:29:00 +0800

    Delete components directory

--- components/ReviewResult.tsx
+++
@@ -1,8 +0,0 @@
-export default function ReviewResult() {
-  return (
-    <div>
-      <label className="block font-medium mb-1">点评结果：</label>
-      <div className="p-4 bg-gray-100 rounded min-h-[120px]">（等待生成...）</div>
-    </div>
-  );
-}
